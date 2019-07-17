546c52ebd5dacc35589b138e265d11627cdb3cb0
Andre Antonio de Castilho Filho <andreacf@jmalucelliti.com.br>
2019-07-17 16:50:26 -0300

    First commit

---
+++ convertDocToPdf/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Office.Interop.Word;
+using System.Configuration;
+
+namespace convertDocToPdf
+{
+    public class Program
+    {
+        public static Microsoft.Office.Interop.Word.Document wordDocument { get; set; }
+        static void Main(string[] args)
+        {
+            var docUrl = ConfigurationManager.AppSettings["docUrl"];
+            var pdfUrl = ConfigurationManager.AppSettings["PdfUrl"];
+
+            Microsoft.Office.Interop.Word.Application appWord = new Microsoft.Office.Interop.Word.Application();
+            wordDocument = appWord.Documents.Open(@docUrl);
+            wordDocument.ExportAsFixedFormat(@pdfUrl, WdExportFormat.wdExportFormatPDF);
+        }
+    }
+}
